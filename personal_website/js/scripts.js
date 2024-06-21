adc7bbdf76a464d7a0cb87bbfe210d7d2f80d07b
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-06-21 08:06:24 +0100

    Home page dynamic

---
+++ personal_website/js/scripts.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Backend Developer", "Data Analytic", "Software Engineer", "Gamer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
